4fe1cc4c04413fe23986d49b9d29a19367453f1c
dt18jjt <57010876+dt18jjt@users.noreply.github.com>
2021-07-20 19:45:40 +0100

    comments added to the button script
    comments were added to portions of the button script to explain their functions

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,13 +7,15 @@ const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({children, type, onclick, buttonStyle, 
     buttonSize}) => { 
+        /*checks the style the button is assigned to, if none it is set to a default style*/
         const checkButtonStyle = STYLES.includes(buttonStyle) ? 
         buttonStyle : STYLES[0]
-
+        /*checks the size the button is assigned to, if none it is set to a default size*/
         const checkButtonSize = SIZES.includes(buttonSize) ? 
         buttonSize : SIZES[0]
 
         return (
+            /*Assigning the sign up button*/
             <Link to='/sign-up' className ='btn-mobile'>
                 <button 
                 className={`btn ${checkButtonStyle} ${checkButtonSize}`}
